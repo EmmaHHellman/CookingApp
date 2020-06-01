3f4b8a642f800ecead67d344bf2cfce18a38b741
Emma Hellman <ehh801@icloud.com>
2020-06-01 11:37:00 -0700

    add coversions

---
+++ script.js
@@ -0,0 +1,25 @@
+function convertToGallon(liter) {
+  var gallon = liter/ 3.785411784;
+  return gallon;
+}
+
+alert(convertToGallon(10));
+
+teaspoons to tablespoon
+
+function convertToTablespoon(teaspoon) {
+var tablespoon = teaspoon/ 3;
+ return tablespoon;
+}
+
+alert(convertToTablespoon(15));
+
+
+convert pints to quarts
+
+function convertToQuart(pint) {
+ var quart = pint/2;
+ return quart;
+}
+
+alert(convertToQuart(40));
